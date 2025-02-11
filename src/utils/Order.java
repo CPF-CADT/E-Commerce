6106070ecbdd63a03582a62843794989d2f7cc0d
RBunna <bunnasovethroath@gmail.com>
2025-02-12 05:32:40 +0700

    Reduce variables from: 	Order.java 	Cart.java

--- src/utils/Order.java
+++ src/utils/Order.java
@@ -1,29 +1,17 @@
 package utils;
 
-
 import java.util.Date;
-import java.util.List;
-import java.util.Map;
 
 public class Order {
   static int orderId = 0;
-  public int userId;
-  public  List<Integer> products; // List of productID in the cart
-  public Map<Integer, Integer> quantities;
-  public  double totalPrice;
-  public  Date orderDate;
-  public  String status;
+  public Cart cart;
+  public Date orderDate = new Date();
+  public String status;
 
-
-  Order(int orderId, int userId, List<Integer> products, Map<Integer, Integer> quantities, Date orderDate, String status) {
+  public Order(Cart cart, String status) {
     orderId = orderId +1;
-    // this.userId = userId;
-    this.products = products;
-    this.quantities = quantities;
-    totalPrice = calculateTotalPrice();
-    // this.orderDate = orderDate;
+    this.cart = cart;
     this.status = status;
   }
-
   
 }
