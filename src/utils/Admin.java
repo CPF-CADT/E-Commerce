8a9a8f7179ca4147a2cd176ad8af0f8774ab0089
Choun Rathanak <Nak4131@gmail.com>
2025-02-12 22:13:15 +0700

    Co-authored-by: Heng <ChhunSivHeng@users.noreply.github.com> Co-authored-by: RBunna <RBunna@users.noreply.github.com> Co-authored-by: satpanha <satpanha@users.noreply.github.com> Co-authored-by: vathanakphy <vathanakphy@gmail.com> Co-authored-by: ChanpanhaCheng <ChanpanhaCheng@users.noreply.github.com>

--- src/utils/Admin.java
+++ src/utils/Admin.java
@@ -4,15 +4,14 @@ import java.util.Date;
 import java.util.HashMap;
 
 public class Admin extends User implements Authentication {
-    private static int counterId = 0;
-    public int adminId;
+    public static int adminId;
     public String position;
 
     public static HashMap<Integer, Admin> adminsById = new HashMap<>();
 
     public Admin(String firstname, String lastname, String email, String password, String address, String phoneNumber, Date dateOfBirth, String position) {
         super(firstname, lastname, email, password, address, phoneNumber, dateOfBirth);
-        this.adminId = counterId +1;
+        adminId++;
         this.position = position;
         adminsById.put(this.adminId, this);
     }
@@ -30,7 +29,7 @@ public class Admin extends User implements Authentication {
 
     // Get total registered admins
     public static int getTotalAdmins() {
-        return counterId;
+        return adminId;
     }
 
     @Override
