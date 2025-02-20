9ca445d92afe63f5edaabe9d225e951f22895dbf
RBunna <bunnasovethroath@gmail.com>
2025-02-20 09:30:49 +0700

    Implement total price in cart

--- src/utils/Cart.java
+++ src/utils/Cart.java
@@ -7,15 +7,15 @@ public class Cart {
   static int idCounter = 0;
   public int cartId;
   int userId;
-  Map<Product, Integer> quantities = new HashMap<Product, Integer>();
+  Map<Integer, Integer> quantities = new HashMap<>();
   double totalPrice = 0.0;
   
-  public Cart(int userId, Map<Product, Integer> quantities) {
+  public Cart(int userId, Map<Integer, Integer> quantities) {
     cartId = ++idCounter;
     this.userId = userId;
     this.quantities = quantities;
-    for (Map.Entry<Product,Integer> entry : quantities.entrySet()) {
-      totalPrice += entry.getKey().price * entry.getValue();
+    for (Map.Entry<Integer, Integer> entry : quantities.entrySet()) {
+      totalPrice += Product.productsById.get(entry.getKey()).price * entry.getValue();
     }
   }
 
